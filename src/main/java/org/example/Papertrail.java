4c2f8865b7062b67f95406c6edc227fd3c4ff3b6
Gr1Lzy <kolomoets02@gmail.com>
2024-01-07 18:44:51 +0200

    fix(Papertrail): replace String with URI for constructor

--- src/main/java/org/example/Papertrail.java
+++ src/main/java/org/example/Papertrail.java
@@ -2,8 +2,10 @@ package org.example;
 
 import com.google.gson.Gson;
 
+import java.net.URI;
+
 public class Papertrail implements Connection {
-    public Papertrail(String uri) {
+    public Papertrail(URI uri) {
 
     }
 
