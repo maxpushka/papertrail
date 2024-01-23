f446489f225db178934e6d22065ba61b38d3d4dd
gr1lzy <kolomoets02@gmail.com>
2024-01-23 21:51:21 +0200

    feat: add func SqlQueryBuilder

--- src/main/java/com/github/papertrail/server/http/querybuilder/adapter/SqlQueryBuilder.java
+++ src/main/java/com/github/papertrail/server/http/querybuilder/adapter/SqlQueryBuilder.java
@@ -4,9 +4,23 @@ import com.github.papertrail.server.http.querybuilder.QueryBuilder;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class SqlQueryBuilder implements QueryBuilder {
     @Override
     public String build(HttpServletRequest requestObject) {
-        return null;
+        StringBuilder requestBody = new StringBuilder();
+        try (BufferedReader reader = requestObject.getReader()) {
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                requestBody.append(line);
+            }
+            return requestBody.toString();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
