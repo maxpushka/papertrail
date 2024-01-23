1e504a212dccb481bc1ced2f19fbae5d7e194764
gr1lzy <kolomoets02@gmail.com>
2024-01-23 21:51:47 +0200

    feat: add func JsonResponseBuilder

--- src/main/java/com/github/papertrail/server/http/responsebuilder/adapter/JsonResponseBuilder.java
+++ src/main/java/com/github/papertrail/server/http/responsebuilder/adapter/JsonResponseBuilder.java
@@ -1,6 +1,7 @@
 package com.github.papertrail.server.http.responsebuilder.adapter;
 
 import com.github.papertrail.server.http.responsebuilder.ResponseBuilder;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 
 import java.util.List;
@@ -8,6 +9,16 @@ import java.util.List;
 public class JsonResponseBuilder implements ResponseBuilder {
     @Override
     public String build(List<JsonElement> input) {
-        return null;
+        if (input == null || input.isEmpty()) {
+            return "[]";
+        }
+
+        JsonArray jsonArray = new JsonArray();
+        for (JsonElement jsonElement : input) {
+            if (jsonElement.isJsonArray()) {
+                jsonArray.add(jsonElement.getAsJsonArray());
+            }
+        }
+        return jsonArray.toString();
     }
 }
